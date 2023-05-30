1262f4ef775b6aaf5eb90900c953d92d4c75ab20
kajela <titoyut56@gmail.com>
2023-05-30 09:07:31 -0400

    add the initial files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Tito  from "./Tito";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Tito />
     </div>
   );
 }
---
+++ src/Tito.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Tito = () => <div>I wana  be competent software engineer someday</div>
+export default Tito;
